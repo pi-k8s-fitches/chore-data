42a758eaecfa6055c16611f04dadf265739e5dbc
Gaffer Fitch <gaffer.fitch@gmail.com>
2019-01-27 10:03:32 -0500

    More accurate setup

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 
 from setuptools import setup, find_packages
 setup(
-    name="pi-k8s-fitches-nandy-data",
-    version="0.5",
-    packages=["pi_k8s_fitches"],
-    package_dir={'pi_k8s_fitches':'lib'},
+    name="nandy-data",
+    version="0.1",
+    packages=["nandy"],
+    package_dir={'':'lib'},
     install_requires=[
-        "redis==2.10.6"
+        "redis==2.10.6",
+        "PyMySQL==0.9.3",
+        "SQLAlchemy==1.2.15",
+        "SQLAlchemy-JSONField==0.7.1",
+        "flask_jsontools==0.1.1-0",
+        "graphyte==1.5"
     ]
 )
